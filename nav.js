542df413a932de58ed94bd5ed1ef3e5ae70acd38
VaultDweller2077 <thuuun@yandex.ru>
2015-10-18 22:10:05 +0300

    init

---
+++ nav.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+    
+    var navof = jQuery("nav").offset().top;     // Как далеко навбар от верхнего края страницы
+    
+    
+    jQuery(window).scroll(function() {
+    var scroll1 = jQuery(window).scrollTop();   // Сколько пикселей отскролили от верха страницы
+        
+        if (scroll1 >= navof) {                 // Сравение 
+            jQuery("nav").addClass("navbar-fixed-top");
+        } else {
+            jQuery("nav").removeClass("navbar-fixed-top");
+        }
+
+
+
+        
+    });
+});
